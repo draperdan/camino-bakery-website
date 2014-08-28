87bca3f5fa3cb188e3eaaeddef73c65f3726c27b
Patrick Beeson <patrickbeeson@gmail.com>
2014-08-28 08:07:16 -0400

    Fix unlinked url on sandwich list template. Finish nav update. Bring in dynamic hours to homepage. Update homepage view.

--- caminobakery/caminobakery/views.py
+++ caminobakery/caminobakery/views.py
@@ -3,19 +3,22 @@ from specials.models import Special
 from hours.models import Schedule
 from menu.models import Item, ItemType
 
-class HomePageView(TemplateView):
-	
-	template_name = "home.html"
 
-	def get_context_data(self, **kwargs):
-		context = super(HomePageView, self).get_context_data(**kwargs)
-		context['special_list'] = Special.today.all()
-		context['hours_list'] = Schedule.objects.all()
-		return context
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['special_list'] = Special.today.all()
+        context['hours_list'] = Schedule.objects.all()
+        return context
+
 
 class RobotsView(TemplateView):
-	
-	template_name = "robots.txt"
-	
-	def render_to_response(self, context, **kwargs):
-		return super(RobotsView, self).render_to_response(context, content_type='text/plain', **kwargs)
+
+    template_name = "robots.txt"
+
+    def render_to_response(self, context, **kwargs):
+        return super(RobotsView, self).render_to_response(
+            context, content_type='text/plain', **kwargs)
