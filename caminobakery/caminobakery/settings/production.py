393cbde14168c03c610cd8ae63ec717a3d10d46e
Patrick Beeson <patrickbeeson@gmail.com>
2015-10-26 20:46:44 -0700

    Add server IP to allowed hosts for now.

--- caminobakery/caminobakery/settings/production.py
+++ caminobakery/caminobakery/settings/production.py
@@ -29,7 +29,7 @@ def get_secret(setting, secrets=secrets):
 
 # HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = ['caminobakery.com']
+ALLOWED_HOSTS = ['159.203.103.24', 'caminobakery.com']
 # END HOST CONFIGURATION
 
 # EMAIL CONFIGURATION
